8dbf3469c2585759c0d6978efe9163f72f1b73cb
Ismoilov713 <muhammadfozilismoilov@gmail.com>
2025-09-24 00:37:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,18 @@
 import os
-import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-# 🔑 Tokenni Railway environment variables ichidan olamiz
+# Tokenni Railway environment variable-dan olamiz
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-
-# /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "♻️ Salom! Bu Ecoback – chiqindini to'g'ri tashlab, cashback yutib oling!"
-    )
+    await update.message.reply_text("♻️ Salom! Bu Ecoback – chiqindini to‘g‘ri tashlab, cashback yutib oling!")
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-
     app.run_polling()
 
 if __name__ == "__main__":
     main()
+
