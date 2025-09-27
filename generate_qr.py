1a7cb773debb8b3e14260142eb1098fdbfa8cc58
Ismoilov713 <muhammadfozilismoilov@gmail.com>
2025-09-27 14:12:59 +0500

    Delete generate_qr.py

--- generate_qr.py
+++
@@ -1,8 +0,0 @@
-# generate_qr.py
-import qrcode
-
-for i in range(1, 11):
-    code = f"ECO{i:03d}"  # ECO001, ECO002, ...
-    img = qrcode.make(code)
-    img.save(f"{code}.png")
-    print(f"✅ {code} yaratildi")
